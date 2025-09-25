3b7112d4bc842321af2289e3b25e932a5d4b491a
YuneidyC <yuneidycastillo25@gmail.com>
2025-09-25 18:00:28 +0100

    Apply format

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
-  const [heading, setHeading] = useState('Hello!')
+    const [heading, setHeading] = useState('Hello!');
 
-  const clickHandler = () => {
-    setHeading('Hello World!')
-  }
+    const clickHandler = () => {
+        setHeading('Hello World!');
+    };
 
-  return (
-    <>
-      <button type="button" onClick={clickHandler}>
-        Click Me
-      </button>
-      <h1>{heading}</h1>
-    </>
-  )
+    return (
+        <>
+            <button type="button" onClick={clickHandler}>
+                Click Me
+            </button>
+            <h1>{heading}</h1>
+        </>
+    );
 }
 
-export default App
+export default App;
