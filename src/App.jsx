322ed9d8dc8119ca31e2686214f19e9a24433e5c
YuneidyC <yuneidycastillo25@gmail.com>
2025-09-29 12:31:52 +0100

    Pull item from API

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
+import { useEffect, useState } from 'react';
+
 import Navbar from './component/Navbar';
 
 import './App.css';
 
 function App() {
+    const serverUrl = 'https://fakestoreapi.com/products';
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        fetch(serverUrl)
+            .then((response) => response.json())
+            .then((data) => setProducts(data))
+    }, []);
+
     return (
         <>
             <Navbar></Navbar>
