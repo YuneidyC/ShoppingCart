6ec54ab2667c1e118f8341d81cfaa9457de5d028
YuneidyC <yuneidycastillo25@gmail.com>
2025-10-01 20:05:42 +0100

    Add function to calculate all cart items

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,17 +1,33 @@
+import { useState } from 'react';
+
 import cart from '../assets/cart.png';
 
 import '../App.css';
 import Cart from './Cart';
 
 function Navbar({ items, addToCart, reduceItem, removeFromCart }) {
+    const [openCart, setOpenCart] = useState(false);
+
+    const totalCartItems = () => {
+        let totalCartItems = 0;
+        items.cart.forEach((item) => {
+            totalCartItems += item.qty;
+        })
+        return totalCartItems;
+    }
+
     return (
         <>
             <div className="navbar">
-                <h1 className="text-black text-[24px]">Shopping Cart</h1>
-                <button className="w-[30px] h-[30px] cursor-pointer">
+                <h1 className="text-black text-[24px]">Shopping Cart </h1>
+                <button className="w-[30px] h-[30px] cursor-pointer" onClick={() => setOpenCart(prev => !prev)}>
                     <img className="w-[30px] h-[30px]" src={cart} alt="cart" />
+                    {items.cart.length > 0 ? (
+                        <div className='w-[20px] h-[20px] text-sm font-semibold absolute top-0 right-4 m-[2px] rounded-[40px] bg-orange-200' >{totalCartItems()}</div>
+                    ) : null}
                 </button>
             </div>
+            {openCart ? <Cart items={items} addToCart={addToCart} reduceItem={reduceItem} removeFromCart={removeFromCart} /> : ''}
         </>
     );
 }
--- src/component/Product.jsx
+++ src/component/Product.jsx
@@ -17,9 +17,6 @@ function Product({ product, addToCart }) {
                     alt={product.title}
                 />
                 <div className="h-auto pl-[5px] pb-[5px] grid grid-row-2 grid-cols-2 items-center">
-                    <p className="w-[180px] h-[23px] row-span-1 col-span-full text-ellipsis whitespace-nowrap overflow-hidden">
-                        {product.title}
-                    </p>
                     <p className="w-[60px] row-span-2 font-bold justify-self-start">
                         ${product.price}
                     </p>
